8b03d715a4db183baa1f35132ed1349d0050aeeb
Jorge Acosta <jorge.acosta@externos.bci.cl>
2020-12-29 22:17:12 -0300

    Add Header interaction

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,17 +1,22 @@
 import '../../style.css'
 import logo from '../../assets/ia_podcast_logo.png'
+import React, { useState } from 'react'
 
 const Header = () => {
+
+    const [selectedIndex, setSelectedIndex ] = useState(0)
+
+    const onClick = index => () => setSelectedIndex(index)
+
+    const menu = ["Home","Podcasts","Nosotros", "Contacto"]
+
     return (
         <header class="header absolute" itemscope itemtype="http://schema.org/Organization">
             <div class="container">
-                <h1 class="logo"><a href="index.html"><img itemprop="logo" src={logo} alt="Imperativo Altruista Podcast" width={163} title="Imperativo Altruista Podcast" /></a></h1>
+                <h1 class="logo"><a href="#"><img itemprop="logo" src={logo} alt="Imperativo Altruista Podcast" width={200} title="Imperativo Altruista Podcast" /></a></h1>
                 <nav class="navigation">
                     <ul class="menu">
-                        <li class="menuitem active"><a href="index.html">Home</a></li>
-                        <li class="menuitem"><a href="#">Podcasts</a></li>
-                        <li class="menuitem"><a href="#">Nosotros</a></li>
-                        <li class="menuitem"><a href="#">Contacto</a></li>
+                        {menu.map((e, index) =>  <li class={`menuitem ${(index === selectedIndex) ? 'active' : ''}`} onClick={onClick(index)}><a href="#">{e}</a></li>)}
                     </ul>
                     <a href="#" class="btn-hamburguer-menu"><i class="fa fa-bars"></i></a>
                 </nav>
